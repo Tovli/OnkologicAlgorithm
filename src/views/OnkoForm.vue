e52740b67e3e41ed6f28d4cd5fdd3dec16b963bd
dekel <dek.asaf@gmail.com>
2018-09-14 10:03:36 +0300

    setting result on submit

--- src/views/OnkoForm.vue
+++ src/views/OnkoForm.vue
@@ -1,5 +1,6 @@
 <template>
 <section>
+    <b-loading :is-full-page="isFullPage" :active.sync="isLoading" :can-cancel="true"></b-loading>
     <b-field label='Patient Id'>
         <b-input v-bind:value="getUserId" rounded disabled> </b-input>
     </b-field>
@@ -21,6 +22,8 @@
     <b-field label='Triglycerides'>
         <b-input v-bind:value="triglycerides" rounded> </b-input>
     </b-field>
+    <button v-on:click="onSubmitForm" class="button is-large is-pull-left">Submit Form</button>
+    <b> {{formSubmitResult}}</b>
     </section>
 </template>
 <script lang="ts">
@@ -35,7 +38,19 @@ export default Vue.extend({
       creatinine: '',
       glucose: '',
       triglycerides: '',
+      isLoading: false,
+      isFullPage: true,
+      formSubmitResult: '',
     };
+  },
+  methods: {
+    onSubmitForm() {
+      this.isLoading = true;
+      setTimeout(() => {
+        this.isLoading = false;
+        this.formSubmitResult = 'Result is 39.5';
+      }, 4 * 1000);
+    },
   },
   props: ['userId'],
   computed: {
