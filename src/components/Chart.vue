6c299442d92cf3395aee0dc67c46d90d1fc0883d
dekel <dek.asaf@gmail.com>
2018-09-14 11:25:34 +0300

    added chartjs (broken)

---
+++ src/components/Chart.vue
@@ -0,0 +1,45 @@
+// <script lang="ts">
+// import Vue from 'vue'
+// import Chart, {Line} from 'vue-chartjs-typescript'
+
+// export default Vue.extend({
+//     extends: Line,
+//     data(){
+//         return {
+//             _chart: '',
+//         };
+//     },
+//     methods: {
+//          renderChart(data: any, options: any) {
+//     let canvas: any = this.$refs.canvas;
+//     this._chart = new Chart(
+//       canvas.getContext("2d"), {
+//         type: this.chartType,
+//         data: data,
+//         options: options,
+//         plugins: this._plugins
+//       }
+//     );
+//   }
+//     },
+//     mounted() {
+//         this.renderChart({
+//             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//             datasets: [
+//                 {
+//                 label: 'Data One',
+//                 backgroundColor: '#FC2525',
+//                 data: [40, 39, 10, 40, 39, 80, 40]
+//                 },{
+//                 label: 'Data Two',
+//                 backgroundColor: '#05CBE1',
+//                 data: [60, 55, 32, 10, 2, 12, 53]
+//                 }
+//             ]
+//             },
+//             {responsive: true, maintainAspectRatio: false});
+//     },
+// });
+// </script>
+
+
---
+++ src/views/DoctorAnalytics.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Chart from 'components/chart.vue'
+export default Vue.extend({
+})
+</script>
