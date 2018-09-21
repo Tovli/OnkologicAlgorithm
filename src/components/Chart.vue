8da03fc66027f4f2832a63aedf61e3319ba71a46
eliranmal <eliranmal@gmail.com>
2018-09-21 23:11:38 +0300

    make the chart work

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,45 +1,52 @@
-// <script lang="ts">
-// import Vue from 'vue'
-// import Chart, {Line} from 'vue-chartjs-typescript'
+<template>
+  <canvas ref="paper"></canvas>
+</template>
 
-// export default Vue.extend({
-//     extends: Line,
-//     data(){
-//         return {
-//             _chart: '',
-//         };
-//     },
-//     methods: {
-//          renderChart(data: any, options: any) {
-//     let canvas: any = this.$refs.canvas;
-//     this._chart = new Chart(
-//       canvas.getContext("2d"), {
-//         type: this.chartType,
-//         data: data,
-//         options: options,
-//         plugins: this._plugins
-//       }
-//     );
-//   }
-//     },
-//     mounted() {
-//         this.renderChart({
-//             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//             datasets: [
-//                 {
-//                 label: 'Data One',
-//                 backgroundColor: '#FC2525',
-//                 data: [40, 39, 10, 40, 39, 80, 40]
-//                 },{
-//                 label: 'Data Two',
-//                 backgroundColor: '#05CBE1',
-//                 data: [60, 55, 32, 10, 2, 12, 53]
-//                 }
-//             ]
-//             },
-//             {responsive: true, maintainAspectRatio: false});
-//     },
-// });
-// </script>
+<script lang="ts">
+import Vue from 'vue';
+import Chart from 'chart.js';
 
-
+export default Vue.extend({
+  data() {
+    return {
+      chart: null,
+      chartType: 'bar',
+      plugins: [],
+    };
+  },
+  methods: {
+    renderChart(data: any, options: any) {
+      const paper = this.$refs.paper as HTMLCanvasElement;
+      this.chart = new Chart(paper.getContext('2d'), {
+        data,
+        options,
+        type: this.chartType,
+        plugins: this.plugins,
+      });
+    },
+  },
+  mounted() {
+    this.renderChart(
+      {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#FC2525',
+            data: [40, 39, 10, 40, 39, 80, 40],
+          },
+          {
+            label: 'Data Two',
+            backgroundColor: '#05CBE1',
+            data: [60, 55, 32, 10, 2, 12, 53],
+          },
+        ],
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: true,
+      },
+    );
+  },
+});
+</script>
