df242697508c8e1682c9a9d07576b8eb901bec99
eliranmal <eliranmal@gmail.com>
2018-09-22 13:19:20 +0300

    make data with random generator

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -6,17 +6,16 @@
 import Vue from 'vue';
 import Chart from 'chart.js';
 
-const random = (max = 100, min = 0) => Math.round(Math.random() * max) - min;
+const random = (ceil = 100, floor = 0) => Math.round(Math.random() * ceil) - floor;
 
-const randoms = (num = 10, eachMax = 100, eachMin = 0) => [...Array(num).keys()]
-  .map(random.bind(null, eachMax, eachMin));
+const randoms = (num = 10, ceil = 100, floor = 0) => [...Array(num).keys()]
+  .map(random.bind(null, ceil, floor));
 
 export default Vue.extend({
   data() {
     return {
       chart: null,
       chartType: 'bar',
-      plugins: [],
     };
   },
   methods: {
@@ -26,24 +25,24 @@ export default Vue.extend({
         data,
         options,
         type: this.chartType,
-        plugins: this.plugins,
       });
     },
   },
   mounted() {
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
     this.renderChart(
       {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels,
         datasets: [
           {
             label: 'Data One',
             backgroundColor: '#ef5575',
-            data: randoms(7),
+            data: randoms(labels.length),
           },
           {
             label: 'Data Two',
             backgroundColor: '#2bc1e0',
-            data: randoms(7),
+            data: randoms(labels.length),
           },
         ],
       },
